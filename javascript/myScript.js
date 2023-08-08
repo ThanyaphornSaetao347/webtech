28e1e4ab598121d3bc61a62a9b027bb1d07a38ef
Thanyaphon347 <tunyaporn5011@gmail.com>
2023-08-08 09:46:54 +0700

    javascript

---
+++ javascript/myScript.js
@@ -0,0 +1,30 @@
+
+function myFunction(){
+    alert("Hello csmju");
+
+    console.log("Hello Monday");
+
+    console.log(document.getElementById("header1").innerText);
+
+    document.getElementById("header1").innerHTML="Change.....";
+    document.getElementById("header1").style.color="#ff0000";
+
+    var x = 8;
+    var y = 20;
+    var z = 3;
+    //หาค่าเฉลี่ย --> header2
+
+    document.getElementById("header2").innerHTML = 
+            ((x + y + z) / 3).toFixed(2);
+
+}
+function likeFunction(){
+    var counter = parseInt(document.getElementById("result").innerText);
+    counter++;
+    document.getElementById("result").innerText = counter;
+    document.getElementById("result").style.fontSize=10+counter+"px";
+    document.getElementById("myIcon").
+}
+function loveFunction(){
+
+}
